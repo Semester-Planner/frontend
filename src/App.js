53739061b870a1bf5033fde50511ebf28ea74986
donnamagi <magi.donna@gmail.com>
2022-09-15 17:55:37 +0200

    feat: querying the backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from "./components/Header";
+import React from "react";
 
 function App() {
+  const [data, setData] = React.useState(null);
 
+  React.useEffect(() => {
+    fetch("/hello")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
   return (
     <div className="container">
-    <Header />
+      <Header />
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }
