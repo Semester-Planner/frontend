54132f39781d55669534ba8298197f2a2dc8aa58
donnamagi <magi.donna@gmail.com>
2022-10-09 15:18:27 +0200

    update: dynamic gallery layout

--- src/components/Modules/Module_view.js
+++ src/components/Modules/Module_view.js
@@ -1,3 +1,4 @@
+import { Row, Col, Container } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import "./Module_view.css";
 
@@ -26,6 +27,7 @@ export const Module = (props) => {
 };
 
 export const ModuleGallery = (props) => {
+  let gallery = [];
   const [modules, setModules] = useState(null);
 
   const [active, setActive] = useState(-1);
@@ -49,34 +51,68 @@ export const ModuleGallery = (props) => {
       .then((modules) => setModules(modules[0].Modules));
   }, []);
 
-  return (
-    <div className="modulesGallery">
-      {modules
-        ? modules.map((module, index) => {
-            return (
-              <Module
-                module={module}
-                key={index}
-                onClick={() => checkActive(index)}
-                active={active === index}
-              />
-            );
-          })
-        : props.modules.map((module, index) => {
-            return (
-              <Module
-                module={module}
-                key={index}
-                onClick={() => checkActive(index)}
-                active={active === index}
-              />
-            );
-          })}
-    </div>
-  );
+  // checks if backend response worked, otherwise seeds view for development
+  modules
+    ? modules.map((module, index) => {
+        return gallery.push(
+          <Module
+            module={module}
+            key={index}
+            onClick={() => checkActive(index)}
+            active={active === index}
+          />
+        );
+      })
+    : props.modules.map((module, index) => {
+        return gallery.push(
+          <Module
+            module={module}
+            key={index}
+            onClick={() => checkActive(index)}
+            active={active === index}
+          />
+        );
+      });
+
+  //dynamic layout for rows and columns
+  let rowCount = Math.floor(gallery.length / props.colCount) + 1;
+  let index = 0;
+
+  const buildGrid = () => {
+    return renderRows();
+  };
+  const renderRows = () => {
+    let rows = [];
+    for (let row = 0; row < rowCount; row++) {
+      rows.push(
+        <Row className="Row" key={row}>
+          {renderCols()}
+        </Row>
+      );
+    }
+    return rows;
+  };
+  const renderCols = () => {
+    let cols = [];
+    for (let col = 0; col < props.colCount; col++) {
+      if (index < gallery.length) {
+        cols.push(
+          <Col key={col} className="Column p-3" md={props.md}>
+            {gallery[index]}
+          </Col>
+        );
+        index++;
+      }
+    }
+    return cols;
+  };
+
+  return <Container className="p-3">{buildGrid()}</Container>;
 };
 
 ModuleGallery.defaultProps = {
+  colCount: 2,
+  md: 6,
   modules: [
     {
       name: "Web Dev",
@@ -89,6 +125,18 @@ ModuleGallery.defaultProps = {
       mod_code: "STS_05",
       department: "STS",
       coordinator: "Fabian Geier",
+    },
+    {
+      name: "Design Methods",
+      mod_code: "ID_06",
+      department: "ID",
+      coordinator: "Pras Gunasekera",
+    },
+    {
+      name: "Design Methods",
+      mod_code: "ID_06",
+      department: "ID",
+      coordinator: "Pras Gunasekera",
     },
     {
       name: "Design Methods",
