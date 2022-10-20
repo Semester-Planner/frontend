451f5b6a44e481b823c567a42962a88d6b670db6
donnamagi <magi.donna@gmail.com>
2022-10-20 19:59:36 +0200

    ref: rendering module gallery in router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./Custom.scss";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ModuleGallery } from "./components/Modules/Module_view";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/modules",
+    element: <ModuleGallery />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  // <BrowserRouter>
+  <React.StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+  // </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
