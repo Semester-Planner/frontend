0b8fff2a23d5f28fe345175d0483a61a204d3829
donnamagi <magi.donna@gmail.com>
2022-10-23 12:38:42 +0200

    feat: google authentication for login

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -19,6 +19,34 @@ export const Login = () => {
 };
 
 export const LoginModal = (props) => {
+  // const loginAPI = () => {
+  //     fetch("/auth/google", {
+  //     method: "GET",
+  //     redirect: 'follow',
+  //     mode: 'no-cors'
+  //     })
+  //     .then((res) => {
+  //         if (res.status !== 200) throw new Error("Server not connected");
+  //         return res.json()
+  //     })
+  //     .then((user) => console.log(user))
+  //     .catch((error) => console.log(error));
+  // }
+  // }
+
+  const logoutAPI = () => {
+    fetch("/auth/logout", {
+      method: "GET",
+      // mode: 'no-cors'
+    })
+      .then((res) => {
+        if (res.status !== 200) throw new Error("Server not connected");
+        return res.json();
+      })
+      .then((modules) => console.log(modules))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <Modal {...props} size="md" aria-labelledby="login-modal" centered>
       <Modal.Header closeButton>
@@ -26,13 +54,20 @@ export const LoginModal = (props) => {
       </Modal.Header>
       <Modal.Body className="text-center pt-5 pb-5">
         <h5 className="pb-3">Make sure to use your CODE email.</h5>
-        <Button id="Google" variant="light">
+        <Button
+          id="Google"
+          variant="light"
+          onClick={() =>
+            window.open(`http://localhost:3001/auth/google`, "_self")
+          }
+        >
           <img
             src="https://www.vectorlogo.zone/logos/google/google-icon.svg"
             alt="G logo"
           />
           Sign in with Google
         </Button>
+        <Button onClick={() => logoutAPI()}>Log out</Button>
       </Modal.Body>
     </Modal>
   );
