4b47cb8a6743a7a972934ba62363b7908477278f
donnamagi <magi.donna@gmail.com>
2022-10-30 21:23:35 +0100

    feat: popup for adding new modules

---
+++ src/components/Modules/Module_new.js
@@ -0,0 +1,79 @@
+import { Button, Modal, Form } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+export const AddModuleButton = () => {
+  const [modalShow, setModalShow] = useState(false);
+
+  return (
+    <div className="d-flex flex-row-reverse">
+      <Button
+        className="rounded-circle m-3"
+        variant="secondary"
+        onClick={() => setModalShow(true)}
+      >
+        +
+      </Button>
+
+      <AddModuleModal show={modalShow} onHide={() => setModalShow(false)} />
+    </div>
+  );
+};
+
+export const AddModuleModal = (props) => {
+  const [modules, setModules] = useState(null);
+  let allModules = [];
+
+  useEffect(() => {
+    fetch("/module/getAllModules", {})
+      .then((res) => {
+        if (res.status !== 200) throw new Error("Server not connected");
+        return res.json();
+      })
+      .then((modules) => setModules(modules))
+      .then(() => (allModules = JSON.parse(JSON.stringify(modules))))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add modules
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="text" placeholder="Search by name" />
+            <Form.Text className="text-muted">Let's hope we have it.</Form.Text>
+          </Form.Group>
+        </Form>
+
+        <div>
+          {modules
+            ? modules.map((module, index) => (
+                <QueryResponse module={module} key={index} />
+              ))
+            : null}
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Save</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export const QueryResponse = (props) => {
+  return (
+    <div className="border p-2 pb-3 m-1">
+      <h5>{props.module.name}</h5>
+      <p>{props.module.department}</p>
+    </div>
+  );
+};
--- src/components/Modules/Module_page.js
+++ src/components/Modules/Module_page.js
@@ -1,5 +1,11 @@
 import { ModuleGallery } from "./Module_gallery";
+import { AddModuleButton } from "./Module_new";
 
 export default function ModulePage() {
-  return <ModuleGallery />;
+  return (
+    <>
+      <AddModuleButton />
+      <ModuleGallery />
+    </>
+  );
 }
