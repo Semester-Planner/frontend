11ea34ac68066fb1debc0e80ba5d24da51063c0b
donnamagi <magi.donna@gmail.com>
2022-12-01 18:39:02 +0100

    feat: unauthorized user test

--- src/Render.test.jsx
+++ src/Render.test.jsx
@@ -1,14 +1,22 @@
-import { render, screen } from "./utils/test-utils";
+import { render, screen, waitFor } from "./utils/test-utils";
 import { CheckAuth } from "./components/Authentication/Login";
+import "whatwg-fetch";
 
 describe("App first paint", () => {
-  it.todo("check login redirect");
+  beforeAll(() => {
+    global.window = Object.create(window);
+    const url = "http://localhost";
+    Object.defineProperty(window, "location", {
+      value: {
+        href: url,
+      },
+      writable: true,
+    });
+  });
 
-  it("shows only navbar", () => {
-    render(<CheckAuth />);
-    expect(screen.getByRole("navigation")).toBeInTheDocument();
-    expect(screen.getByText(/The Semester Planner/i)).toBeInTheDocument();
-    expect(screen.getByText(/Log out/i)).toBeInTheDocument();
-    expect(screen.queryByRole("heading")).not.toBeInTheDocument();
+  it("redirects to login if user is unauthorized", async () => {
+    await render(<CheckAuth />); // MSW returns 403
+    await expect(screen.queryByRole("navigation")).not.toBeInTheDocument();
+    await waitFor(() => expect(window.location.href).toBe("/login"));
   });
 });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,16 +2,13 @@ import { rest } from "msw";
 
 export const handlers = [
   // Handles a "GET /auth/session" request
-  rest.get("http://localhost:3001/auth/session", (req, res, ctx) => {
+  rest.get("http://localhost/auth/session", (req, res, ctx) => {
     console.log("hehehehehhe");
-    return res(ctx.status(200));
+    return res(ctx.status(403));
   }),
 
-  rest.get(
-    "http://localhost:3001/module/getAllUserModules",
-    (req, res, ctx) => {
-      console.log("hehehehehhe");
-      return res(ctx.status(403));
-    }
-  ),
+  rest.get("http://localhost/module/getAllUserModules", (req, res, ctx) => {
+    console.log("hehehehehhe");
+    return res(ctx.status(403));
+  }),
 ];
