c05c91639a2f4a51a7cb640d7bc0790e8883dc5b
donnamagi <magi.donna@gmail.com>
2022-12-04 11:20:25 +0100

    feat: module component tests with json response

--- src/components/Modules/Module.test.jsx
+++ src/components/Modules/Module.test.jsx
@@ -1,12 +1,33 @@
-import { render, screen, waitFor } from "../../utils/test-utils";
-import ModulePage from "./Module_page";
+import { fireEvent, render, screen, waitFor } from "../../utils/test-utils";
+import userEvent from "@testing-library/user-event";
 import "whatwg-fetch";
 
-describe("Modules page", () => {
-  it("renders modules page", async () => {
-    await render(<ModulePage />);
-    await waitFor(() =>
-      expect(screen.getByText(/Missing something?/i)).toBeInTheDocument()
-    );
-  });
+import ModulePage from "./Module_page";
+import { SearchModal } from "./Module_new";
+
+describe("Module interactions", () => {
+  it("renders modules page", () => {
+    const { findByText } = render(<ModulePage />);
+    expect(findByText(/Missing something?/i));
+  }),
+    it("opens a modal component", async () => {
+      const { getByText, findByPlaceholderText } = render(<ModulePage />);
+      const addModulesButton = getByText(/Add modules/i);
+      await userEvent.click(addModulesButton);
+      expect(findByPlaceholderText(/Search by name/i));
+    }),
+    it("shows 5 first search results", async () => {
+      const { getByText } = render(<SearchModal show={true} />);
+
+      await waitFor(() => {
+        expect(getByText(/Composition/i));
+        expect(getByText(/Teamwork and/i));
+        expect(getByText(/Leadership/i));
+        expect(getByText(/Creative Problem/i));
+        expect(getByText(/Self-Directed/i));
+      });
+
+      const results = screen.getAllByRole("button"); // all modules render as buttons
+      expect(results.length).toBe(6); // 5 modules + 1 close button
+    });
 });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -7,6 +7,63 @@ export const handlers = [
   }),
 
   rest.get("http://localhost/module/getAllUserModules", (req, res, ctx) => {
-    return res(ctx.status(403));
+    return res(ctx.status(200));
+  }),
+
+  rest.get("http://localhost/module/getAllModules", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: "5a437f87-1bc2-417b-95c7-403f26743235",
+          name: "Composition",
+          mod_code: "ID_01_v2",
+          department: "ID",
+          coordinator: "Martin Knobel",
+        },
+        {
+          id: "3e972210-7d06-483b-aeb0-0836627a6df4",
+          name: "Teamwork and Collaboration",
+          mod_code: "IS_01_v2",
+          department: "IS",
+          coordinator: "Britte Loeckel",
+        },
+        {
+          id: "b893c61b-9d7e-421e-a35e-2517c802665b",
+          name: "Leadership",
+          mod_code: "IS_02_v2",
+          department: "IS",
+          coordinator: "Britte Loeckel",
+        },
+        {
+          id: "8b3b01be-82af-4664-bcb0-0e8ac58ad5d3",
+          name: "Creative Problem-Solving",
+          mod_code: "IS_03_v2",
+          department: "IS",
+          coordinator: "Martin Knobel",
+        },
+        {
+          id: "179e00dd-0ef5-4f87-886f-b76c94482a6b",
+          name: "Self-Directed (Curiosity-Driven) Learning",
+          mod_code: "IS_04_v2",
+          department: "IS",
+          coordinator: "Adam Roe",
+        },
+        {
+          id: "49164345-8b97-4fc5-a09b-82e32d526780",
+          name: "Sustainable Development",
+          mod_code: "IS_05_v2",
+          department: "IS",
+          coordinator: "Florian Grote",
+        },
+        {
+          id: "b4c2350d-dafa-4b41-aef1-046e0cf2a1c0",
+          name: "Intercultural Understanding",
+          mod_code: "IS_06_v2",
+          department: "IS",
+          coordinator: "Fabian Geier",
+        },
+      ])
+    );
   }),
 ];
