ec0f465c758e899e981c7d95813fc407af47b302
donnamagi <magi.donna@gmail.com>
2022-12-06 09:30:11 +0100

    ref: rendering module page before each interaction test

--- src/components/Modules/Module.test.jsx
+++ src/components/Modules/Module.test.jsx
@@ -5,14 +5,17 @@ import "whatwg-fetch";
 import ModulePage from "./Module_page";
 
 describe("Module interactions", () => {
+  beforeEach(() => {
+    render(<ModulePage />);
+  });
+
   it("renders modules page", () => {
-    const { findByText } = render(<ModulePage />);
-    expect(findByText(/Missing something?/i));
-  }),
-    it("opens a modal component", async () => {
-      const { getByText, findByPlaceholderText } = render(<ModulePage />);
-      const addModulesButton = getByText(/Add modules/i);
-      await userEvent.click(addModulesButton);
-      expect(findByPlaceholderText(/Search by name/i));
-    });
+    expect(screen.findByText(/Missing something?/i));
+  });
+
+  it("opens a modal component", async () => {
+    const addModulesButton = screen.getByText(/Add modules/i);
+    await userEvent.click(addModulesButton);
+    expect(screen.findByPlaceholderText(/Search by name/i));
+  });
 });
